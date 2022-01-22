865ee4bf7466f636df1b7aefb4f3f4a72a56b66e
Alberto <alberto-city@hotmail.com>
2022-01-22 14:17:46 -0600

    added delete and complete todos

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -4,13 +4,19 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="list_item">
-      <p className={`item icon icon_check ${props.completed && 'icon_check--active'}`}>
+      <p 
+        className={`item icon icon_check ${props.completed && 'icon_check--active'}`}
+        onClick={props.onComplete}
+      >
         <b>√</b>
       </p>
       <p className={`item item_text ${props.completed && 'item_text--complete'}`}>
         {props.text}
       </p>
-      <p className="item icon icon_delete">
+      <p 
+        className="item icon icon_delete"
+        onClick={props.onDelete}
+      >
         <b>X</b>
       </p>
     </li>
