b9767bd6eeb28f2fe2cca767c6c4590be1a3e3c3
Alberto <alberto-city@hotmail.com>
2022-01-22 21:56:24 -0600

    added new hook and local storage logic

--- src/App.js
+++ src/App.js
@@ -5,16 +5,36 @@ import {TodoList} from './components/TodoList/TodoList';
 import {TodoItem} from './components/TodoItem/TodoItem';
 import {TodoButton} from './components/TodoButton/TodoButton';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'llegar a challenger', completed: false },
-];
+function useLocalStorage(itemName, initialValue) {
+
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = react.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  }
+
+  return [
+    item,
+    saveItem
+  ];
+
+}
 
 function App() {
   
-  const [todos, setTodos] = react.useState(defaultTodos);
+  const [todos, setTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = react.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length; 
